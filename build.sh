a52080251ad57f70743c1061c27f74712af73bda
Paolo Bosetti <paolo.bosetti@unitn.it>
2015-05-22 09:06:47 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+VERSION=$(date +"%y.%m.%d")
+PATCHLEVEL=`(cd mruby; git rev-parse --short HEAD)`
+SOURCE_BIN=./mruby/bin
+
+echo "Building deb package for version $VERSION-$PATCHLEVEL"
+
+mkdir -p products
+mkdir -p spool
+mkdir -p spool/usr/bin
+mkdir -p spool/DEBIAN
+echo
+
+echo "Copying Binaries"
+cp $SOURCE_BIN/mruby spool/usr/bin/mruby
+cp $SOURCE_BIN/mrbc spool/usr/bin/mrbc
+cp $SOURCE_BIN/mirb spool/usr/bin/mirb
+#cp $SOURCE_BIN/mruby-config spool/usr/bin/mruby-config
+echo
+
+echo "Setting up permissions"
+find ./spool -type d | xargs chmod 755
+echo
+
+echo "Creting metainformation"
+echo "Package: mruby
+Version: $VERSION-p$PATCHLEVEL
+Section: base
+Priority: optional
+Architecture: armhf
+Depends: libc6, libreadline6, libssl1.0.0, libpcre3, libyaml-0-2
+Maintainer: Matteo Ragni <matteo.ragni@unitn.it
+Description: mruby-mechatronics for makeropen. Please remember to install wiringPi" > spool/DEBIAN/control
+echo
+cat spool/DEBIAN/control
+echo
+
+echo "Building package"
+fakeroot dpkg-deb --build spool
+mv spool.deb products/mruby-unitn-$VERSION-$PATCHLEVEL.deb
+echo "Completed"
+
+rm -rf spool
---
+++ run_test.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+if __FILE__ == $PROGRAM_NAME
+  require 'fileutils'
+  unless File.exists?('mruby')
+    system 'git clone --depth 1 https://github.com/mruby/mruby.git mruby'
+  end
+  exit system(%Q[cd mruby; MRUBY_CONFIG=#{File.expand_path __FILE__} ./minirake #{ARGV.join(' ')}])
+end
+
+MRuby::Build.new do |conf|
+  toolchain :gcc
+  conf.gembox 'default'
+  # conf.gem File.dirname(__FILE__)
+  conf.cc.defines = %w(ENABLE_READLINE)
+  conf.gem "mruby/mrbgems/mruby-time"
+  conf.gem :github => 'pbosetti/mruby-io', :branch => 'master'
+  conf.gem :github => 'pbosetti/mruby-dir', :branch => 'master'
+  conf.gem :github => 'pbosetti/mruby-tempfile', :branch => 'master'
+  conf.gem :github => 'pbosetti/mruby-pcre-regexp', :branch => 'master' 
+  conf.gem :github => 'pbosetti/mruby-yaml', :branch => 'master'
+  conf.gem :github => 'pbosetti/mruby-merb', :branch => 'master'
+  conf.gem :github => 'pbosetti/mruby-complex', :branch => 'master'
+  conf.gem :github => 'pbosetti/mruby-serialport', :branch => 'master'
+  conf.gem :github => 'pbosetti/mruby-shell', :branch => 'master'
+  conf.gem :github => 'pbosetti/mruby-sleep', :branch => 'master'
+  conf.gem :github => 'iij/mruby-pack', :branch => 'master'
+  conf.gem :github => 'iij/mruby-socket', :branch => 'master'
+  conf.gem :github => 'iij/mruby-errno', :branch => 'master'
+  conf.gem :github => 'UniTN-mechatronics/mruby-ftp', :branch => 'master'
+  conf.gem :github => 'UniTN-mechatronics/mruby-raspberry', :branch => 'master'
+  conf.gem :github => 'UniTN-Mechatronics/mruby-fsm', :branch => 'master'
+  conf.gem :github => 'pbosetti/mruby-emb-require', :branch => 'master'
+end
