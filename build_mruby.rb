cc0250308de80527a74a0432303dc53ae23f6604
Paolo Bosetti <paolo.bosetti@unitn.it>
2015-06-16 17:09:38 +0200

    Fixed compilation

--- build_mruby.rb
+++ build_mruby.rb
@@ -13,13 +13,12 @@ puts "*** USING CUSTOM MRUBY CONFIG #{__FILE__} ***"
 MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
-  # conf.gem File.dirname(__FILE__)
-  conf.cc.defines = %w(ENABLE_READLINE)
+  conf.cc.defines += %w(ENABLE_READLINE)
   conf.gem "mruby/mrbgems/mruby-time"
   conf.gem :github => 'pbosetti/mruby-io', :branch => 'master'
   conf.gem :github => 'pbosetti/mruby-dir', :branch => 'master'
   conf.gem :github => 'pbosetti/mruby-tempfile', :branch => 'master'
-  conf.gem :github => 'pbosetti/mruby-pcre-regexp', :branch => 'master' 
+  conf.gem :github => 'pbosetti/mruby-pcre-regexp', :branch => 'master'
   conf.gem :github => 'pbosetti/mruby-yaml', :branch => 'master'
   conf.gem :github => 'pbosetti/mruby-merb', :branch => 'master'
   conf.gem :github => 'pbosetti/mruby-complex', :branch => 'master'
@@ -30,14 +29,13 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'iij/mruby-socket', :branch => 'master'
   conf.gem :github => 'iij/mruby-errno', :branch => 'master'
   conf.gem :github => 'iij/mruby-process', :branch => 'master'
-  conf.gem :github => "ksss/mruby-signal"
+  conf.gem :github => 'ksss/mruby-signal', :branch => 'master'
   conf.gem :github => 'mattn/mruby-thread', :branch => 'master'
   conf.gem :github => 'UniTN-mechatronics/mruby-ftp', :branch => 'master'
   conf.gem :github => 'UniTN-mechatronics/mruby-raspberry', :branch => 'master'
   conf.gem :github => 'UniTN-Mechatronics/mruby-fsm', :branch => 'master'
   conf.gem :github => 'UniTN-Mechatronics/mruby-kalman', :branch => 'master'
   conf.gem :github => 'UniTN-Mechatronics/mruby-mrubot', :branch => 'master'
-  conf.gem :github => "UniTN-Mechatronics/mruby-raspicam", :branch => 'master'
-  #conf.gem :github => "take-cheeze/mruby-marshal"
+  conf.gem :github => 'UniTN-Mechatronics/mruby-raspicam', :branch => 'master'
   conf.gem :github => 'pbosetti/mruby-emb-require', :branch => 'master'
 end
